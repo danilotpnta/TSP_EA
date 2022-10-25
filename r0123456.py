3a29433c72f9dfaae1ffb02c39c5a2aa170b5ebb
hkotesova <hkotes@g.ucla.edu>
2022-10-25 14:44:01 +0200

    Order crossover function implemented

--- r0123456.py
+++ r0123456.py
@@ -29,6 +29,60 @@ class TSP_problem:
 		#print(distance)
 		return distance
 
+	def recombination(self, ind1, ind2):
+		subset_indices = np.random.randint(low=0, high=len(ind1.order), size=2)
+		print("indices:", subset_indices)
+		low_index = np.min(subset_indices)
+		high_index = np.max(subset_indices)
+		subset_first_parent = ind1.order[low_index:high_index+1]
+		print("first parent: ", ind1.order)
+		print("second parent: ", ind2.order)
+		print("subset of first parent: ", subset_first_parent)
+		offspring = []
+		# j is the index of ind2 that marks the element being placed in the offspring
+		j = 0
+		# i is the index of the offspring where an element of ind2 is being placed
+		for i in range(0, low_index):
+			print(j)
+			ind2_value = ind2.order[j]
+
+			# if the element in ind2 is already in the random subset of the first parent,
+			# then skip that element and iterate to the next one
+			while ind2_value in subset_first_parent and j < len(ind1.order)-1:
+				j += 1
+				ind2_value = ind2.order[j]
+
+			if j >= len(ind1.order):
+				break
+
+			offspring.append(ind2_value)
+			j += 1
+
+		# now add the subset from the first parent
+		offspring += subset_first_parent
+		print(j)
+
+		# now do the same loop on ind2 for the remainder of the offspring indices
+		for i in range(high_index+1, len(ind1.order)):
+			print(j)
+			ind2_value = ind2.order[j]
+
+			# if the element in ind2 is already in the random subset of the first parent,
+			# then skip that element and iterate to the next one
+			while ind2_value in subset_first_parent and j < len(ind1.order)-1:
+				j += 1
+				ind2_value = ind2.order[j]
+
+			if j >= len(ind1.order):
+				break
+
+			offspring.append(ind2_value)
+			j += 1
+
+		print("offspring: ", offspring)
+		return offspring
+
+
 class Individual:
 	def __init__(self, order=None, alpha=None):
 		if order == None:
@@ -56,11 +110,12 @@ class r0123456:
 		# print(type(distanceMatrix[5,7]))
 
 		TSP = TSP_problem(distanceMatrix)
-		ind = Individual([0, 1, 2, 3],0.5)
-		TSP_fitness= TSP.fitness(ind)
+		ind = Individual( [0, 1, 2, 3, 4, 5, 6, 7],0.5)
+		ind2 = Individual([7, 6, 1, 3, 4, 0, 5, 2], 0.5)
+		TSP.recombination(ind, ind2)
 
 		# Your code here.
-		yourConvergenceTestsHere = True
+		yourConvergenceTestsHere = False
 		while( yourConvergenceTestsHere ):
 			meanObjective = 0.0
 			bestObjective = 0.0
@@ -83,6 +138,7 @@ class r0123456:
 
 a = r0123456()
 a.optimize('./tour50.csv')
+
 
 # ind = Individual([0, 1, 2, 3],0.5)
 # a.fitness(ind)
